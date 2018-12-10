663219c293adbbd6884849dc44e374ae25c04e94
Samantha <samareimorgan@gmail.com>
2018-12-09 20:55:44 -0700

    Revert "Update onclick"
    This reverts commit 2d49c5a946f131818bda859a8143e41a8d5042a1.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,7 +6,7 @@ function NavBar(props) {
     
     <nav className="navbar sticky-top navbar-light">
         <a className="clickTitle" >Fraggle Rock Clicky Game</a>
-        <h3 className="message">{props.message}</h3>
+        <h3 className="clickguess">{props.clickGuess}</h3>
         <h3>Score: {props.currentScore} | Top Score: {props.topScore}</h3>
     </nav>
     
